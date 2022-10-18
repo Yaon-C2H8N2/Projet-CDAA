859d4186763fe671a57d1ed714c66eb5648b1c63
C2H8N2 <y.dusoleil@outlook.com>
2022-10-18 09:39:37 +0200

    Ajout commentaires

--- src/GestionTache/GestionTache.cpp
+++ src/GestionTache/GestionTache.cpp
@@ -4,18 +4,40 @@
 
 #include "GestionTache.h"
 
+/**
+ * Ajoute à la liste une copie de l'instance de Tache passée en paramètre.
+ * @param t
+ * Une Tache à ajouter à la liste.
+ */
 void GestionTache::addTache(Tache t) {
     this->listeTache.push_back(t);
 }
 
+/**
+ * Supprime toute instance de la tache passée en paramètre.
+ * @param t
+ * Une Tache à supprimer de la liste.
+ */
 void GestionTache::removeTache(Tache t) {
     this->listeTache.remove(t);
 }
 
+/**
+ * Retourne le nombre d'instance de Tache enregistrée dans la liste.
+ * @return
+ * Un int décrivant le nombre de Tache dans la liste.
+ */
 int GestionTache::getNbTache() {
     return this->listeTache.size();
 }
 
+/**
+ * Retourne la Tache à un indice spécifique dans la liste
+ * @param i
+ * Indice de la Tache à renvoyer.
+ * @return
+ * Une copie de l'instance Tache dans la liste.
+ */
 Tache GestionTache::getTache(int i) {
     int cpt = 0;
     Tache res;
--- src/Tache/Tache.cpp
+++ src/Tache/Tache.cpp
@@ -4,36 +4,80 @@
 
 #include "Tache.h"
 
+/**
+ * Définit le nom de la Tache.
+ * @param nomTache
+ * Un nom sous forme de std::string.
+ */
 void Tache::setNomTache(string nomTache) {
     this->nomTache = nomTache;
 }
 
+/**
+ * Retourne le nom de cette instance de Tache.
+ * @return
+ * Le nom de cette instance de Tache sous forme de std::string.
+ */
 string Tache::getNomTache() {
     return this->nomTache;
 }
 
+/**
+ * Définit le contenu de cette instnace de Tache.
+ * @param contenu
+ * Un contenu sous forme de std::string.
+ */
 void Tache::setContenu(string contenu) {
     this->contenu = contenu;
 }
 
+/**
+ * Retourne le contenu de cette instance de Tache.
+ * @return
+ * Le contenu de cette instance de Tache sous forme de std::string.
+ */
 string Tache::getContenu() {
     return this->contenu;
 }
 
+/**
+ * Définit la date à laquelle la Tache doit être réalisée.
+ * @param dateTache
+ * Une structure tm dont la copie sera définie comme date à laquelle la Tache doit être réalisée. Une copie est nécessaire car localtime() retournera toujours le même pointeur.
+ */
 void Tache::setDateTache(tm dateTache) {
     this->dateTache = dateTache;
 }
 
+/**
+ * Retourne la date à laquelle la Tache doit être réalisée.
+ * @return
+ * Un pointeur vers une structure tm symbolisant la date à laquelle la Tache doit être réalisée.
+ */
 tm Tache::getDateTache() {
     return this->dateTache;
 }
 
+/**
+ * Redéfinition de l'opérateur d'égalité afin de comparer les tâches entre elles.
+ * @param rhs
+ * Une instance de Tache avec laquelle comparer l'instance actuelle.
+ * @return
+ * bool : True si les deux instances de Tache sont égales. sinon false.
+ */
 bool Tache::operator==(Tache rhs) {
     return nomTache == rhs.nomTache &&
            contenu == rhs.contenu &&
            mktime(&dateTache) == mktime(&rhs.dateTache);
 }
 
+/**
+ * Redéfinition de l'opérateur d'inégalité afin de comparer les tâches entre elles.
+ * @param rhs
+ * Une instance de Tache avec laquelle comparer l'instance actuelle.
+ * @return
+ * bool : True si les deux instances de Tache sont inégales. sinon false.
+ */
 bool Tache::operator!=(Tache rhs) {
     return !(rhs == *this);
 }
